450994cceed8a0d01cecb13d43f9fde6a992e115
Brigid Prinsloo <b.i.prinsloo@gmail.com>
2015-07-12 17:22:04 +0200

    Seeded users and wikis.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create an admin user
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+  )
+admin.skip_confirmation!
+admin.save!
+
+# Create a premium user
+premium = User.new(
+  name: 'Premium User',
+  email: 'premium@example.com',
+  password: 'helloworld',
+  role: 'premium'
+  )
+premium.skip_confirmation!
+premium.save!
+
+# Create a standard user
+standard = User.new(
+  name: 'Standard User',
+  email: 'standard@example.com',
+  password: 'helloworld',
+  role: 'standard'
+  )
+standard.skip_confirmation!
+standard.save!
+
+# Create Users
+5.times do 
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all 
+
+# Create Wikis
+50.times do 
+  Wiki.create!(
+    user: users.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+    )
+end
+
+puts "Seed finished"
+puts "Created admin, premium and standard user"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
